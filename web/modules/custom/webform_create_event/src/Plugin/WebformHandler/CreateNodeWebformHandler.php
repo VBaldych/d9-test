3bd6fcd36bcb49595044fb81a972673aa64e22e5
VBaldych <vovabaldych@gmail.com>
2022-11-25 15:57:21 +0200

    Added possibility to choose node type in custom webform handler

--- web/modules/custom/webform_create_event/src/Plugin/WebformHandler/CreateNodeWebformHandler.php
+++ web/modules/custom/webform_create_event/src/Plugin/WebformHandler/CreateNodeWebformHandler.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\webform_create_event\Plugin\WebformHandler;
 
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\node\Entity\Node;
+use Drupal\node\Entity\NodeType;
 use Drupal\webform\Plugin\WebformHandlerBase;
 use Drupal\webform\WebformSubmissionInterface;
 
@@ -24,10 +26,62 @@ class CreateNodeWebformHandler extends WebformHandlerBase {
   /**
    * {@inheritdoc}
    */
+  public function defaultConfiguration() {
+    return [
+      'choose_node_type' => '',
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    $node_types = $this->getNodeTypes();
+    $form['choose_node_type'] = [
+      '#type' => 'select',
+      '#title' => t('Content type'),
+      '#default_value' => $this->configuration['choose_node_type'],
+      '#options' => $node_types,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * Retrieves node types.
+   *
+   * @return array
+   *   The result array.
+   */
+  protected function getNodeTypes() {
+    $node_types = NodeType::loadMultiple();
+    $result = [];
+    foreach ($node_types as $node_type) {
+      $result[$node_type->id()] = $node_type->label();
+    }
+
+    return $result;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    $this->configuration['choose_node_type'] = $values['choose_node_type'];
+
+    parent::submitConfigurationForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function postSave(WebformSubmissionInterface $webform_submission, $update = TRUE) {
     $values = $webform_submission->getData();
     $node_args = [
-      'type' => 'article',
+      'type' => $this->configuration['choose_node_type'],
       'langcode' => 'en',
       'created' => time(),
       'changed' => time(),
