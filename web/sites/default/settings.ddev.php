a8625c04e46f665bbc06bd4d7005a13ac1f18773
VBaldych <vovabaldych@gmail.com>
2023-01-03 23:59:08 +0200

    Fixed errors from reports/status page

--- web/sites/default/settings.ddev.php
+++ web/sites/default/settings.ddev.php
@@ -57,3 +57,5 @@ $config['swiftmailer.transport']['transport'] = 'smtp';
 $config['swiftmailer.transport']['smtp_host'] = '127.0.0.1';
 $config['swiftmailer.transport']['smtp_port'] = '1025';
 $config['swiftmailer.transport']['smtp_encryption'] = '0';
+
+$settings['rebuild_access'] = FALSE;
